6e652e0e7f59a51ccb189069f095d2dc0a0abe6a
Marcel Pi <marcelpi97@gmail.com>
2023-10-01 14:42:37 +0200

    Add file io example.

--- CompileTimeIO/Main.cpp
+++ CompileTimeIO/Main.cpp
@@ -1,3 +1,5 @@
+#include <array>
+#include <cstdio>
 
 
 
@@ -8,4 +10,27 @@ constexpr char build_path[] = BUILD_PATH;
 
 int main()
 {
+    constexpr char test_text[] =
+        "A\n"
+        "B\n"
+        "C\n"
+        "D\n";
+    
+    constexpr char path[] = SOURCE_PATH "/test_file.txt";
+    constexpr auto path_size = sizeof(path) - 1;
+
+    if constexpr (!__builtin_file_exists(path, path_size))
+        (void)__builtin_file_write(path, path_size, test_text, sizeof(test_text) - 1);
+    
+    constexpr auto read_text = [&]() constexpr
+    {
+        constexpr auto size = __builtin_file_size(path, path_size);
+        std::array<char, size> data = { };
+        __builtin_file_read(path, path_size, data.data(), data.size());
+        return data;
+    }();
+
+    fwrite(read_text.data(), 1, read_text.size(), stdout);
+
+    return 0;
 }
