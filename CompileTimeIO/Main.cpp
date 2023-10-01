8f75a9604374c1ad7142f2ed32ef652c7ac4a016
Marcel Pi <marcelpi97@gmail.com>
2023-10-01 19:05:10 +0200

    Minor tweaks.

--- CompileTimeIO/Main.cpp
+++ CompileTimeIO/Main.cpp
@@ -22,12 +22,19 @@ int main()
     if constexpr (!__builtin_file_exists(path, path_size))
         (void)__builtin_file_write(path, path_size, test_text, sizeof(test_text) - 1);
     
-    constexpr auto read_text = [&]() constexpr
+    constexpr auto read_text = [=]() consteval
     {
-        constexpr auto size = __builtin_file_size(path, path_size);
-        std::array<char, size> data = { };
-        __builtin_file_read(path, path_size, data.data(), data.size());
-        return data;
+        if constexpr (__builtin_file_exists(path, path_size))
+        {
+            constexpr auto size = __builtin_file_size(path, path_size);
+            std::array<char, size> data = { };
+            __builtin_file_read(path, path_size, data.data(), data.size());
+            return data;
+        }
+        else
+        {
+            return std::array<char, 0>();
+        }
     }();
 
     fwrite(read_text.data(), 1, read_text.size(), stdout);
